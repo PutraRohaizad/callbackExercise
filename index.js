447185c4e918790115995db81330fb9da2731b48
PutraRohaizad <putrarohayzad@gmail.com>
2019-09-10 11:18:06 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// function B(){
+//     setTimeout(() => {
+//         console.log('world');
+//     },500);
+// }
+// function A(post,callback){
+//     setTimeout(() =>{
+//         console.log('hello');
+//         callback();
+//     }, 200)
+// }
+
+// A('post',B);
+
+const workers = [{name:'Ali', position}]
